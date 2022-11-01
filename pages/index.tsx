7e609943dafa533c5a7337e0b0bdb952f203bbef
guijun13 <guilhermejun13.grillo@gmail.com>
2022-11-01 00:12:24 -0300

    add main search box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
-import { LocalActivity, LocalFireDepartment, Luggage, Person } from '@mui/icons-material';
+import {
+  AirlineSeatReclineNormal,
+  CalendarMonth,
+  CompareArrows,
+  FlightLand,
+  FlightTakeoff,
+  LocalActivity,
+  LocalFireDepartment,
+  Luggage,
+  PeopleAlt,
+  Person,
+} from '@mui/icons-material';
 import HomeIcon from '@mui/icons-material/Home';
-import { Button, IconButton } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  TextField,
+} from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -8,13 +28,13 @@ import Logo from '../public/logo.svg';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen flex-col">
       <Head>
         <title>Voe</title>
         <link rel="icon" href="/logo.ico" />
       </Head>
 
-      <header className="flex justify-between w-full px-8 py-6">
+      <header className="flex justify-between w-full px-8 py-6 top-0">
         <div>
           <Image alt="Voe logo" src={Logo} />
         </div>
@@ -40,8 +60,86 @@ const Home: NextPage = () => {
         </div>
       </header>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1></h1>
+      <main className="flex items-center justify-around mx-4 px-8 py-4 rounded-xl bg-purple text-white">
+        <div className="flex flex-col items-center mx-4">
+          <div className="flex">
+            <div className="flex flex-col">
+              <div className="flex pb-4">
+                <FlightTakeoff />
+                <p className="ml-4">Origem</p>
+              </div>
+              <TextField id="outlined-basic" label="Insira a cidade de origem" variant="outlined" />
+            </div>
+            <CompareArrows className="flex mx-4" />
+            <div className="flex flex-col">
+              <div className="flex pb-4">
+                <FlightLand />
+                <p className="ml-4">Destino</p>
+              </div>
+              <TextField
+                id="outlined-basic"
+                label="Insira a cidade de destino"
+                variant="outlined"
+              />
+            </div>
+          </div>
+
+          <FormControl className="mt-4">
+            <RadioGroup
+              row
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="ida e volta"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel value="ida e volta" control={<Radio />} label="Ida e volta" />
+              <FormControlLabel value="somente ida" control={<Radio />} label="Somente ida" />
+              <FormControlLabel value="multidestinos" control={<Radio />} label="Multidestinos" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+
+        <div className="flex items-center  mx-4">
+          <div className="flex flex-col">
+            <div className="flex pb-4">
+              <CalendarMonth />
+              <p className="ml-4">Ida</p>
+            </div>
+            <TextField id="outlined-basic" label="Insira a data de ida" variant="outlined" />
+          </div>
+          <div className="flex flex-col">
+            <div className="flex pb-4">
+              <CalendarMonth />
+              <p className="ml-4">Volta</p>
+            </div>
+            <TextField id="outlined-basic" label="Insira a data de volta" variant="outlined" />
+          </div>
+        </div>
+
+        <div className="flex items-center mx-4">
+          <div className="flex flex-col">
+            <div className="flex pb-4">
+              <PeopleAlt />
+              <p className="ml-4">Passageiros</p>
+            </div>
+            <TextField id="outlined-select-seat-class" select label="Insira os passageiros">
+              <MenuItem value={'Economica'}>1 adulto</MenuItem>
+            </TextField>
+          </div>
+        </div>
+
+        <div className="flex items-center mx-4">
+          <div className="flex flex-col">
+            <div className="flex pb-4">
+              <AirlineSeatReclineNormal />
+              <p className="ml-4">Classe</p>
+            </div>
+            <TextField id="outlined-select-seat-class" select label="Insira sua classe">
+              <MenuItem value={'Economica'}>Economica</MenuItem>
+            </TextField>
+          </div>
+        </div>
+
+        <div className="flex"></div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t"></footer>
