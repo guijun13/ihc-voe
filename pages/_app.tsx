cb7d3cbd895a279fac6abcc53962c58602c0ab37
guijun13 <guilhermejun13.grillo@gmail.com>
2022-12-05 10:09:40 -0300

    add vlibras

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '../styles/checkout.css';
+import VLibras from 'vlibras-nextjs';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <VLibras forceOnload />
+    </>
+  );
 }
 
 export default MyApp;
